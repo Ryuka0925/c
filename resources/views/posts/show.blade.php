4dc9505dfb8811ef9af59f10fde26be1ebc33d65
Ryuka0925 <22m20211@gmail.com>
2022-10-30 11:04:35 +0900

    編集

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -17,8 +17,10 @@
                 <p>{{ $post->body }}</p>
             </div>
         </div>
+        <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
         <div class="footer">
             <a href="/">戻る</a>
         </div>
+        
     </body>
  </html>
